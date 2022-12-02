e16e4d4f992cfebee4d820d92677785614194a58
marcel <marcel.taubert@epitech.eu>
2022-12-02 16:43:14 +0000

    Refactor

--- day2.asm
+++ day2.asm
@@ -1,9 +1,23 @@
+; nasm -felf64 day2.asm && ld day2.o -o day2 && ./day2
+
 %define STDIN 0
 %define STDOUT 1
 %define STDERR 2
 
 %define SYS_WRITE 1
 %define SYS_EXIT 60
+
+%define AVAL 65
+%define BVAL 66
+%define CVAL 67
+%define XVAL 88
+%define YVAL 89
+%define ZVAL 90
+
+%define ROCK 65
+%define PAPER 66
+%define SCISSORS 67
+
 
 section .bss                  ; 
   digitSpace resb 100         ; Reserve bytes for debug print
@@ -75,9 +89,18 @@ _start:
     push rbp
     mov rbp, rsp
 
-    ;call part1
+    call part1
+
+    mov rax, r8
+    call debug_print
+
     call part2
 
+    call exit
+
+; ============================================================================
+; Part 2
+; ============================================================================
 part2:
     xor r8, r8      ; total 
     xor r9, r9      ; pos in string
@@ -86,7 +109,7 @@ part2:
 
 loop2:
     cmp r9, data_len
-    jge exit
+    jge return
 
     ; get the next char of data
     mov r10b, [data + r9]
@@ -100,12 +123,12 @@ loop2:
     add r9, 2 ; skip the \n
 
 add_to_r8_2:
-    cmp r11b, 88
+    cmp r11b, XVAL
     je need_loss
 
     ; add 3 for draw
     add r8, 3
-    cmp r11b, 89
+    cmp r11b, YVAL
     je need_draw
 
     ; add 6 for win
@@ -113,26 +136,26 @@ add_to_r8_2:
     jmp need_win
 
 need_loss:
-    cmp r10b, 65 ; rock
+    cmp r10b, ROCK
     je need_scissors
-    cmp r10b, 66 ; paper
+    cmp r10b, PAPER
     je need_rock
-    cmp r10b, 67 ; scissors
+    cmp r10b, SCISSORS
     je need_paper
 
 need_draw:
-    cmp r10b, 65 ; rock
+    cmp r10b, ROCK
     je need_rock
-    cmp r10b, 66 ; paper
+    cmp r10b, PAPER
     je need_paper
-    cmp r10b, 67 ; scissors
+    cmp r10b, SCISSORS
     je need_scissors
 need_win:
-    cmp r10b, 65 ; rock
+    cmp r10b, ROCK
     je need_paper
-    cmp r10b, 66 ; paper
+    cmp r10b, PAPER
     je need_scissors
-    cmp r10b, 67 ; scissors
+    cmp r10b, SCISSORS
     je need_rock
 
 need_rock:
@@ -145,92 +168,91 @@ need_scissors:
     add r8, 3
     jmp loop2
     
+; ============================================================================
+; Part 1
+; ============================================================================
 
-; part1:
-;     xor r8, r8      ; total 
-;     xor r9, r9      ; pos in string
-;     xor r10, r10    ; enemy move
-;     xor r11, r11    ; my move
-; 
-; loop:
-;     cmp r9, data_len
-;     jge exit
-; 
-;     ; get the next char of data
-;     mov r10b, [data + r9]
-; 
-;     ; add 2 to get to the next char and skip the space
-;     add r9, 2
-; 
-;     ; move that char into r11
-;     mov r11b, byte [data + r9]
-; 
-;     add r9, 2 ; skip the \n
-; 
-;     ;call add_to_r8
-;     ;jmp loop
-; 
-; ; value should be in r11
-; add_to_r8:
-;     ; add 1 for rock
-;     add r8, 1
-; 
-;     ;push rax
-;     ;mov al, r10b
-;     ;call debug_print
-;     ;pop rax
-; 
-;     cmp r11b, 88
-;     je rock
-; 
-;     ; add 1 for paper
-;     add r8, 1
-;     cmp r11b, 89
-;     je paper
-; 
-;     ; add 1 for scissors
-;     add r8, 1
-;     jmp scissors
-;     ; ret
-;     ; jmp loop
-; 
-; rock:
-;     cmp r10b, 66 ; paper = loss
-;     je loss
-; 
-;     cmp r10b, 65 ; rock = draw
-;     je draw
-; 
-;     cmp r10b, 67 ; scissors = win
-;     je win
-; 
-; paper:
-;     cmp r10b, 66 ; paper = draw
-;     je draw
-; 
-;     cmp r10b, 65 ; rock = win
-;     je win
-; 
-;     cmp r10b, 67 ; scissors = loss
-;     je loss
-; 
-; scissors:
-;     cmp r10b, 66 ; paper = win
-;     je win
-; 
-;     cmp r10b, 65 ; rock = loss
-;     je loss
-; 
-;     cmp r10b, 67 ; scissors = draw
-;     je draw
-; win:
-;     add r8, 6
-;     jmp loop
-; draw:
-;     add r8, 3
-;     jmp loop
-; loss:
-;     jmp loop
+part1:
+    xor r8, r8      ; total 
+    xor r9, r9      ; pos in string
+    xor r10, r10    ; enemy move
+    xor r11, r11    ; my move
+
+loop:
+    cmp r9, data_len
+    jge return
+
+    ; get the next char of data
+    mov r10b, [data + r9]
+
+    ; add 2 to get to the next char and skip the space
+    add r9, 2
+
+    ; move that char into r11
+    mov r11b, byte [data + r9]
+
+    add r9, 2 ; skip the \n
+
+    ;call add_to_r8
+    ;jmp loop
+
+; value should be in r11
+add_to_r8:
+    ; add 1 for rock
+    add r8, 1
+
+    cmp r11b, XVAL
+    je rock
+
+    ; add 1 for paper
+    add r8, 1
+    cmp r11b, YVAL
+    je paper
+
+    ; add 1 for scissors
+    add r8, 1
+    jmp scissors
+
+rock:
+    cmp r10b, PAPER
+    je loss
+
+    cmp r10b, ROCK
+    je draw
+
+    cmp r10b, SCISSORS
+    je win
+
+paper:
+    cmp r10b, PAPER
+    je draw
+
+    cmp r10b, ROCK
+    je win
+
+    cmp r10b, SCISSORS
+    je loss
+
+scissors:
+    cmp r10b, PAPER
+    je win
+
+    cmp r10b, ROCK
+    je loss
+
+    cmp r10b, SCISSORS
+    je draw
+win:
+    add r8, 6
+    jmp loop
+draw:
+    add r8, 3
+    jmp loop
+loss:
+    jmp loop
+
+return:
+    ret
 
 exit:
     mov rax, r8
